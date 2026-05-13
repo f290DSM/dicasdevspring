1a70c89f58c9e257c52c0767d0db9ab98faa6893
Esdras B Silva <bzr4@icloud.com>
2026-05-13 20:05:36 -0300

    Type List

--- src/main/java/dev/sdras/dicasdevspring/resources/CategoriaResource.java
+++ src/main/java/dev/sdras/dicasdevspring/resources/CategoriaResource.java
@@ -5,11 +5,14 @@ import dev.sdras.dicasdevspring.dto.CategoriaDTO;
 import dev.sdras.dicasdevspring.repositories.CategoriaRepository;
 import jakarta.validation.Valid;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.modelmapper.internal.bytebuddy.description.type.TypeList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.lang.reflect.Type;
 import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,6 +25,8 @@ public class CategoriaResource {
 
     private final ModelMapper modelMapper;
 
+    Type listType = new TypeToken<List<CategoriaDTO>>() {}.getType();
+
     public CategoriaResource(CategoriaRepository repository, ModelMapper modelMapper) {
         this.repository = repository;
         this.modelMapper = modelMapper;
@@ -30,32 +35,20 @@ public class CategoriaResource {
     @GetMapping
     public ResponseEntity<List<CategoriaDTO>> findAll() {
         List<CategoriaEntity> categories = repository.findAll();
-        if (categories.isEmpty()) {
-            return ResponseEntity.notFound().build();
-        }
-
-        List<CategoriaDTO> categorias = categories
-                .stream()
-                .map(entity -> new CategoriaDTO(
-                        entity.getId(),
-                        entity.getDescricao())
-                ).collect(Collectors.toList());
+        if (categories.isEmpty())  return ResponseEntity.notFound().build();
+        List<CategoriaDTO> categorias = modelMapper.map(categories, listType);
         return ResponseEntity.ok(categorias);
     }
 
     @PostMapping
     public ResponseEntity<Void> save(@Valid @RequestBody CategoriaDTO dto) {
-        //TODO: Criar mapeamento dinamico com modelmapper
-        CategoriaEntity categoriaEntity = new CategoriaEntity();
-        categoriaEntity.setDescricao(dto.getDescricao());
-        CategoriaEntity entity = repository.save(categoriaEntity);
-
+        CategoriaEntity entity = repository
+                .save(modelMapper.map(dto, CategoriaEntity.class));
         URI uri = ServletUriComponentsBuilder
                 .fromCurrentRequest()
                 .path("/{id}")
                 .buildAndExpand(entity.getId())
                 .toUri();
-
         return ResponseEntity.created(uri).build();
     }
 }
